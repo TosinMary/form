1ef6b73db3b4ee9a2a4043c84cb19aa0f56ac25a
TosinMary <teetoastl3@gmail.com>
2023-04-30 19:05:42 +0100

    show password feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -8,9 +9,22 @@ import * as yup from 'yup';
 
 
 const App = () =>  {
+
+  const checkPassword = () => {
+   let x = document.getElementById("exampleInputPassword1");
+   let y = document.getElementById("exampleInputPassword2");
+   if (x.type === "password" & y.type === "password") {
+    x.type = "text";
+    y.type = "text";
+   }else {
+    x.type = "password";
+    y.type = "password";
+   }
+  }
+
   const schema = yup.object().shape({
     fullName: yup.string().required("Your fullName is required"),
-    age: yup.number().positive().integer().min(18).required(),
+    age: yup.number().positive().integer().min(18).required("Age must be above 18"),
     email: yup.string().email().required(),
     password: yup.string().min(4).max(20).required(),
     confirmPassword: yup.string().oneOf([yup.ref("password")]).required()
@@ -50,13 +64,13 @@ const App = () =>  {
     <p className='text-danger'>{errors.password?.message} </p>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Confirm password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"   {...register("confirmPassword")}/>
+    <label for="exampleInputPassword2" class="form-label">Confirm password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2"   {...register("confirmPassword")}/>
     <p className='text-danger'>{errors.confirmPassword?.message} </p>
   </div>
   <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <input type="checkbox" onClick={checkPassword} class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1">Show Password</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
